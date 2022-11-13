7936ebd7818ce6031e6495de5b3572d510a122e3
Moaz Ahmed <moaz@troylab.org>
2022-11-13 23:35:20 +0200

    optimize: Recipe Card

--- lib/widgets/recipe_card.dart
+++ lib/widgets/recipe_card.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:recipes/models/recipe.dart';
 
 class RecipeCard extends StatelessWidget {
-  const RecipeCard({super.key, required this.imageUrl, required this.label});
+  const RecipeCard({
+    super.key,
+    required this.recipe,
+  });
 
-  final String imageUrl;
-  final String label;
+  final Recipe recipe;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          Image(
-            image: AssetImage(
-              imageUrl,
+      //1
+      elevation: 2.0,
+      //2
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage(
+                recipe.imageUrl,
+              ),
             ),
-          ),
-          Text(label),
-        ],
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
