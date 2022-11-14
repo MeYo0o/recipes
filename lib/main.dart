abc56c5d4d97d15adbcec3b00df0387e4968c9b6
Moaz Ahmed <justmeyo2025@gmail.com>
2022-11-14 20:16:17 +0200

    init ch3

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:recipes/models/recipe.dart';
-import 'package:recipes/screens/recipe_detail.dart';
-import 'package:recipes/widgets/recipe_card.dart';
 
 void main() {
-  runApp(const RecipeApp());
+  // 1
+  runApp(const Fooderlich());
 }
 
-class RecipeApp extends StatelessWidget {
-  const RecipeApp({super.key});
-
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({super.key});
   @override
   Widget build(BuildContext context) {
-    final ThemeData themeData = ThemeData();
-
+    // TODO: Create theme
+    // TODO: Apply Home widget
+    // 3
     return MaterialApp(
-      title: 'Recipe Calculator',
-      debugShowCheckedModeBanner: false,
-      theme: themeData.copyWith(
-        colorScheme: themeData.colorScheme.copyWith(
-          primary: Colors.grey,
-          secondary: Colors.black,
-        ),
-      ),
-      home: const MyHomePage(title: 'Recipe Calculator'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // 1
-    return Scaffold(
-      // 2
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      // 3
-      body: SafeArea(
-        // 4
-        child: ListView.builder(
-          // 5
-          itemCount: Recipe.samples.length,
-          // 6
-          itemBuilder: (context, index) {
-            // 7
-            return GestureDetector(
-              // 8
-              onTap: () {
-                // 9
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      // 10
-
-                      return RecipeDetail(
-                        recipe: Recipe.samples[index],
-                      );
-                    },
-                  ),
-                );
-              },
-              child: RecipeCard(
-                recipe: Recipe.samples[index],
-              ),
-            );
-          },
-        ),
+      // TODO: Add theme
+      title: 'Fooderlich',
+      // 4
+      home: Scaffold(
+        // TODO: Style the title
+        appBar: AppBar(title: const Text('Fooderlich')),
+        // TODO: Style the body text
+        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
       ),
     );
   }
