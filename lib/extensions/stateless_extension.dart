cb021bb3de63fbf1938e35d60fba8a5846a3fbc8
Moaz Ahmed <justmeyo2025@gmail.com>
2022-11-15 22:43:56 +0200

    add: extensions to get the applied theme for any widget in the Widget Tree.

---
+++ lib/extensions/stateless_extension.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+extension StatelessExtention on StatelessWidget {
+  ThemeData extensionTheme(BuildContext context) {
+    return Theme.of(context);
+  }
+}
