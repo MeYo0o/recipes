aaea88033cc15961269b7af0acbf64784ee4fd05
Moaz Ahmed <justmeyo2025@gmail.com>
2022-12-26 14:54:49 +0200

    init ch19 : Final Chapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,25 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:logging/logging.dart';
-import 'package:provider/provider.dart';
-
-// import 'data/memory_repository.dart';
-// import 'data/sqlite/sqlite_repository.dart';
-import 'data/moor/moor_repository.dart';
-import 'data/repository.dart';
-import 'network/recipe_service.dart';
-import 'network/service_interface.dart';
-import 'ui/main_screen.dart';
+import 'ui/message_list.dart';
 
 Future<void> main() async {
-  _setupLogging();
   WidgetsFlutterBinding.ensureInitialized();
-  // final repository = SqliteRepository();
-  final repository = MoorRepository();
-  await repository.init();
-  runApp(MyApp(repository: repository));
+  // TODO: Add Firebase Initialization
+  runApp(const App());
 }
 
-void _setupLogging() {
-  Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen(
-    (rec) {
-      log('${rec.level.name}: ${rec.time}: ${rec.message}');
-    },
-  );
-}
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  final Repository repository;
-
-  const MyApp({
-    Key? key,
-    required this.repository,
-  }) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<Repository>(
-          lazy: false,
-          create: (_) => repository,
-          dispose: (_, Repository repository) => repository.close(),
-        ),
-        Provider<ServiceInterface>(
-          // create: (_) => MockService()..create(),
-          create: (_) => RecipeService.create(),
-          lazy: false,
-        ),
-      ],
-      child: MaterialApp(
-        title: 'Recipes',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColor: Colors.white,
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: const MainScreen(),
-      ),
+    // TODO: Add MultiProvider
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'RayChat',
+      theme: ThemeData(primaryColor: const Color(0xFF3D814A)),
+      // TODO: Add Consumer<UserDao> here
+      home: const MessageList(),
+      // TODO: Add closing parenthesis
     );
   }
 }
